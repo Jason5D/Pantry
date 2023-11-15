32b059c2382f3244d78b0450d4ea47de31abe23b
Jason <jason5donoghue@gmail.com>
2023-11-15 20:19:59 +0000

    adding search function

---
+++ Pantry/Views/Item/ShowSearchForm.cshtml.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Pantry.Data;
+using Pantry.Models;
+
+namespace Pantry.Views
+{
+    public class ShowSearchFormModel : PageModel
+    {
+        private readonly Pantry.Data.ApplicationDbContext _context;
+
+        public ShowSearchFormModel(Pantry.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Item Item { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Item == null || Item == null)
+            {
+                return Page();
+            }
+
+            _context.Item.Add(Item);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
